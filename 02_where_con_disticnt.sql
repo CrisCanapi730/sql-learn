4d7b5248640fcfa9799668a03abbd870e285b113
Cristian Canapi <cristiancanapi730@gmail.com>
2025-09-21 18:23:31 -0400

    Select con distinct y cambio de nombre del primer archivo

---
+++ 02_where_con_disticnt.sql
@@ -0,0 +1,13 @@
+-- sentencia select
+
+-- Todos los datos de una tabla
+SELECT * FROM usuarios;
+
+-- Solo los nombres de los usuarios
+SELECT nombre FROM usuarios;
+
+-- Ver nombres con apellidos de los usuarios
+SELECT nombre, apellido FROM usuarios;
+
+SELECT DISTINCT edad FROM usuarios;
+
